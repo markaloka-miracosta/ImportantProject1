6d533e9be47adef13721ff4b0f4232504699c7df
Mark Aloka <maloka@miracosta.edu>
2020-08-24 12:34:55 -0700

    Add project files.

---
+++ Part2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("        *        ");
+            Console.WriteLine("       ***       ");
+            Console.WriteLine("      *****      ");
+            Console.WriteLine("     *******     ");
+            Console.WriteLine("    *********    ");
+        }
+    }
+}
